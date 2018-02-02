ada86178f4b380cb48aa9c1ac573fce9e0f21db0
Eric Zoerner <eric.zoerner@nike.com>
2018-02-02 13:31:01 -0800

    partial solution through Set 1 / More Generators

--- MonadChallenge.hsproj/Set1.hs
+++ MonadChallenge.hsproj/Set1.hs
@@ -3,16 +3,31 @@
 
 module Set1 where
   
-import MCPrelude (Seed, rand, mkSeed, toLetter)
+import MCPrelude  (Seed, rand, mkSeed, toLetter)
 
 fiveRands :: [Integer]
 fiveRands = take 5 $ drop 1 $ map fst $ iterate (\(_, seed) -> rand seed) (0, mkSeed 1)
     
+type Gen a = Seed -> (a, Seed)
 
-randLetter :: Seed -> (Char, Seed)
+randLetter :: Gen Char
 randLetter seed = case rand seed of
   (n , seed) -> (toLetter n, seed)
 
 randString3 :: String
 randString3 = take 3 $ drop 1 $ map fst  $
   iterate (\(_, seed) -> randLetter seed) ('0', mkSeed 1)
+
+
+randEven :: Gen Integer -- the output of rand * 2
+randEven = generalA (* 2)
+
+randOdd :: Gen Integer -- the output of rand * 2 + 1
+randOdd = generalA $ (+ 1) . (* 2)
+
+randTen :: Gen Integer -- the output of rand * 10
+randTen = generalA (* 10)
+
+generalA :: (Integer -> Integer) -> Gen Integer
+generalA f seed = case rand seed of
+  (n, sd) -> (f n, sd)
