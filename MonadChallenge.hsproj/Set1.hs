1f2e5d79bd5d620d7cc643989ec5320c88c48ecf
Eric Zoerner <eric.zoerner@nike.com>
2018-02-08 17:13:09 -0800

    rework generalA

--- MonadChallenge.hsproj/Set1.hs
+++ MonadChallenge.hsproj/Set1.hs
@@ -16,23 +16,25 @@ randLetter :: Gen Char
 randLetter seed = case rand seed of
   (n , seed) -> (toLetter n, seed)
 
--- 3. More Generators
 randString3 :: String
 randString3 = take 3 $ drop 1 $ map fst  $
   iterate (\(_, seed) -> randLetter seed) ('0', mkSeed 1)
 
-
+-- 3. More Generators
 randEven :: Gen Integer -- the output of rand * 2
-randEven = generalA (* 2)
+randEven = generalA id (* 2)
 
 randOdd :: Gen Integer -- the output of rand * 2 + 1
-randOdd = generalA $ (+ 1) . (* 2)
+randOdd = generalA id $ (+ 1) . (* 2)
 
 randTen :: Gen Integer -- the output of rand * 10
-randTen = generalA (* 10)
+randTen = generalA id (* 10)
 
-generalA :: (Integer -> Integer) -> Gen Integer
-generalA f seed = case rand seed of
-  (n, sd) -> (f n, sd)
+generalA :: (a -> a) -> (Integer -> a) -> Gen a
+generalA f fromInt seed = case rand seed of
+  (n, sd) -> (f $ fromInt n, sd)
   
+generalA' :: (a -> b) -> Gen a -> Gen b
+generalA' a2b genA seed = undefined -- ??
+
 -- 4. Generalizing Random Pairs
