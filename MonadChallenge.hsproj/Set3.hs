506789be2aa4da75ddf09c1c78f219b00479238f
Eric Zoerner <eric.zoerner@gmail.com>
2018-04-18 09:50:11 -0700

    attempt at solving 3.4 and 3.5

--- MonadChallenge.hsproj/Set3.hs
+++ MonadChallenge.hsproj/Set3.hs
@@ -36,8 +36,8 @@ allCards1 ranks suits =
    
 
 -- 3. Generalizing pairs and cards
-allCombs :: (a -> b -> c) -> [a] -> [b] -> [c]
-allCombs f as bs =
+allCombs1 :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs1 f as bs =
   let
     next [] _ = []
     next (_ : xs) [] = next xs bs
@@ -47,7 +47,40 @@ allCombs f as bs =
 
 
 allPairs :: [a] -> [b] -> [(a,b)]
-allPairs = allCombs (,)
+allPairs = allCombs1 (,)
 
 allCards :: [Int] -> [String] -> [Card]
-allCards = allCombs Card
+allCards = allCombs1 Card
+
+-- 4. Combinations of three things
+allCombs34 :: (a -> b -> c -> d) -> [a] -> [b] -> [c] -> [d]
+allCombs34 f as bs cs =
+  let
+    next [] _ _ = []
+    next (_ : xs) [] _ = next xs bs cs
+    next xs (_ : ys) [] = next xs ys cs
+    next xs@(x : _) ys@(y : _) (z : zs) = f x y z : next xs ys zs
+  in
+    next as bs cs
+
+-- 5. Combinations of more things
+combStep :: [a -> b] -> [a] -> [b]
+combStep fs as =
+  let
+    next [] _ = []
+    next (_ : fs) [] = next fs as
+    next fs@(f : _) (y : ys) = f y : next fs ys
+  in
+    next fs as
+      
+allCombs :: (a -> b -> c) -> [a] -> [b] -> [c]
+allCombs f as bs =
+  let
+    next [] _ = []
+    next (_ : xs) [] = next xs bs
+    next xs@(x : _) (y : ys) = f x y : next xs ys
+  in
+    next as bs
+  
+
+
